6eeb9f9f649617adadcee54d989ed5d7481b6403
Sam <elucidation@gmail.com>
2011-11-28 05:19:52 -0500

    added basic word frequency count

--- wordStats.rb
+++ wordStats.rb
@@ -31,12 +31,14 @@ end
 ARGV.each do |filename|
   numLines = 0
   numChars = 0
+  words = Hash.new(0)
   begin
     timeElapsed = Benchmark.realtime do
     f = File.open(filename,'r')
       puts "Performing letter/word statistics on file '#{filename}'"
       puts "Number of characters per line: " if options[:verbose]
       while line = f.gets
+        line.chomp! # Remove trailing endline character \n
         if options[:verbose]
           puts "#{line.length} : #{line}"
         else
@@ -44,11 +46,22 @@ ARGV.each do |filename|
         end
         numLines += 1
         numChars += line.length
+        if line.length > 0
+          cleanLine = line.scan(/[A-Za-z](?:\w)+|(?:[A-Za-z]\.)+[A-Za-z]?/)
+          puts "#{cleanLine.length} words :: #{cleanLine.join(' ')}" if options[:verbose]
+          cleanLine.each do |word|
+            words[word] += 1
+          end
+        end
+        puts 
       end
       f.close
     end
     puts
     puts "Number of lines: #{numLines}, Number of characters: #{numChars}"
+    words.reject! {|k,v| v <= 1}
+    puts "Word count (2+ occurences, ignore single-letter words): #{words}"
+    puts "Most frequent word: #{words.key(words.values.max)}"
     puts "Time elapsed: #{timeElapsed}s"
     puts "Finished reading file '#{filename}'\n\n"
   rescue => err
